6c61b60c3b10e66e045699f7a996c9129abb4cfe
your name <lionelhrithik@gmail.com>
2025-06-14 15:59:12 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+// function sum(a,b){
+//     console.log(a+b);
+    
+// }
+// sum(10,20)
+    
+// var sum = ()=>{
+//     var a=10;
+//     var b=20;
+//     console.log(a+b);
+    
+// }
+// sum();
+var marks =[91,52,63,94,85]
+var [m1,m2,m3,m4,m5]=marks;
+console.log(m1);
+console.log(m2);
+console.log(m3);
+console.log(m4);
+console.log(m5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
